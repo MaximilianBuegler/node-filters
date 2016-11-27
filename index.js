62f8f8cf572f9eb55f44a86d493130032036e7d8
Maximilian Bügler <max@maxbuegler.eu>
2016-11-27 21:39:34 +0100

    Fixed ,

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = {
     median: require('./lib/median'),
     average: require('./lib/average'),
     average2D: require('./lib/average2D'),
-    aggregate: require('./lib/aggregate'),
+    aggregate: require('./lib/aggregate')
 };
 
